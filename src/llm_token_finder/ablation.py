e8d6ff58919d8e4c79ed4d1c08a541ba5f9a0cb6
Marc North <contact@marcnorth.co.uk>
2025-05-31 14:38:01 +0100

    make AblationLlm static methods private

--- src/llm_token_finder/ablation.py
+++ src/llm_token_finder/ablation.py
@@ -40,7 +40,7 @@ class AblationLlm:
             heads_by_layer[head.layer].append(head)
         for layer, layer_heads in heads_by_layer.items():
             hook_name = f"blocks.{layer}.attn.hook_v"
-            hook_fn = functools.partial(self.ablate_attention_head, heads_to_ablate=layer_heads)
+            hook_fn = functools.partial(self._ablate_attention_head, heads_to_ablate=layer_heads)
             self.llm.add_hook(hook_name, hook_fn)
 
     def _add_token_movement_ablation_hooks(self, token_movement_to_ablate: list[tuple[int | Token, int | Token]]) -> None:
@@ -51,11 +51,11 @@ class AblationLlm:
         # Add the hook to every layer, as we want to ablate the movement between tokens for every attention head in every layer.
         for layer_index in range(self.llm.cfg.n_layers):
             hook_name = f"blocks.{layer_index}.attn.hook_attn_scores"
-            hook_fn = functools.partial(self.ablate_token_movement, token_movement_to_ablate=token_movement_to_ablate)
+            hook_fn = functools.partial(self._ablate_token_movement, token_movement_to_ablate=token_movement_to_ablate)
             self.llm.add_hook(hook_name, hook_fn)
 
     @staticmethod
-    def ablate_attention_head(
+    def _ablate_attention_head(
             activation: Float[Tensor, "batch sequence_length n_head d_head"],
             hook: HookPoint,
             heads_to_ablate: list[AttentionHead]
@@ -68,7 +68,7 @@ class AblationLlm:
         return activation
 
     @staticmethod
-    def ablate_token_movement(
+    def _ablate_token_movement(
             activation: Float[Tensor, "batch n_head sequence_length_q sequence_length_k"],
             hook: HookPoint,
             token_movement_to_ablate: list[tuple[int, int]]
