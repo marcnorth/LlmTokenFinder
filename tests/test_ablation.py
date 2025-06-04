d074e42d992552282002de77510601cb9af31a32
Marc North <contact@marcnorth.co.uk>
2025-06-04 09:20:42 +0100

    add test for ablation logits

--- tests/test_ablation.py
+++ tests/test_ablation.py
@@ -60,3 +60,35 @@ class AblationTest(unittest.TestCase):
                 # Check that the attention pattern for the head is all zeros between 'quick' and 'fox' with ablation
                 self.assertFalse(torch.all(ablated_attention_pattern == 0))
                 self.assertTrue(torch.all(ablated_attention_pattern[fox_token.index][quick_token.index] == 0))
+
+    def test_ablation_logits(self):
+        # If we ablate the movement from a token to every subsequent token, that token should not affect the logits of subsequent tokens.
+        ablation_llm = AblationLlm(self.llm)
+        # WET
+        word_to_ablate = "wet"
+        text = f"I have a {word_to_ablate} shirt. My shirt is"
+        token_finder = TokenFinder.create_from_tokenizer(text, self.llm.tokenizer)
+        tokens = self.llm.tokenizer.tokenize(text, add_special_tokens=True)
+        from_token = token_finder.find_first(word_to_ablate, allow_space_prefix=True)
+        # Get unablated logits with "wet"
+        wet_unablated_logits, _ = ablation_llm.forward(text)
+        # Get ablated logits with "wet"
+        to_ablate = [(from_token.index, i) for i in range(from_token.index+1, len(tokens))]  # Ablate movement from token index 4 to indices 5-9
+        wet_ablated_logits, _ = ablation_llm.forward(text, token_movement_to_ablate=to_ablate)
+        self.assertFalse(torch.equal(wet_unablated_logits, wet_ablated_logits))
+        # TIGHT
+        word_to_ablate = "tight"
+        text = f"I have a {word_to_ablate} shirt. My shirt is"
+        token_finder = TokenFinder.create_from_tokenizer(text, self.llm.tokenizer)
+        tokens = self.llm.tokenizer.tokenize(text, add_special_tokens=True)
+        from_token = token_finder.find_first(word_to_ablate, allow_space_prefix=True)
+        # Get unablated logits with "tight"
+        tight_unablated_logits, _ = ablation_llm.forward(text)
+        # Get ablated logits with "tight"
+        to_ablate = [(from_token.index, i) for i in range(from_token.index+1, len(tokens))]  # Ablate movement from token index 4 to indices 5-9
+        tight_ablated_logits, _ = ablation_llm.forward(text, token_movement_to_ablate=to_ablate)
+        self.assertFalse(torch.equal(tight_unablated_logits, tight_ablated_logits))
+        # Ablated logits should not be affected "wet" or "tight", so they should be the same everywhere except for the token being ablated.
+        self.assertFalse(torch.all(wet_ablated_logits[:, from_token.index] == tight_ablated_logits[:, from_token.index]))
+        self.assertTrue(torch.all(wet_ablated_logits[:, :from_token.index] == tight_ablated_logits[:, :from_token.index]))
+        self.assertTrue(torch.all(wet_ablated_logits[:, from_token.index + 1:] == tight_ablated_logits[:, from_token.index + 1:]))
